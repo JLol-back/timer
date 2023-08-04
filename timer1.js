a69b662052548e80b5aa189c404e157cad7eb372
Jake Lollback <jalollback@gmail.com>
2023-08-03 19:06:22 -0500

    Experimenting with continue statements to handle edge cases in loop

--- timer1.js
+++ timer1.js
@@ -8,12 +8,18 @@ if (process.argv.length <= 2) {
 for (let i = 2; i < process.argv.length; i++) {
  
 //console.log(typeof process.argv[i]);
-////let test = Number(process.argv[i]);
+console.log(Number(process.argv[i]));
 //console.log(typeof test);
 //console.log(process.argv[i]);
 
-if ((Number(process.argv[i]) === Number) && (Number(process.argv[i]) >= 0)); {
-   let delay = (process.argv[i]) * 1000;
+  if (Number(process.argv[i]) === NaN) {
+    continue;
+  }
+
+  if (Number(process.argv[i]) < 0) {
+    continue;
+  }
+
+  let delay = (process.argv[i]) * 1000;
     setTimeout(() => { process.stdout.write('\x07')}, delay);
-  };
 };
